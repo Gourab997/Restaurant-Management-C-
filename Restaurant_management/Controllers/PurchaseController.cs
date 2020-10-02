3496f0cf70722de4d79cf8539d492d46453309e1
Gourab997 <Gourabg998@gmail.com>
2020-10-02 23:11:46 +0600

    Final Update

---
+++ Restaurant_management/Controllers/PurchaseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restaurant_management.Models;
+namespace Restaurant_management.Controllers
+{
+    public class PurchaseController
+    {
+        static Database db = new Database();
+
+        public static void NewPurchases(string Name, string Address, string Phone,string  Customer_name)
+        {
+            Purchase c = new Purchase();
+            c.FName = Name;
+            c.Phone = Phone;
+            c.Address = Address;
+            c.Customer_name = Customer_name;
+
+            db.Purchases.NewPurchases(c);
+        }
+        public static ArrayList GetAllOrder()
+        {
+            return db.Purchases.GetAllOrder();
+        }
+
+       
+    }
+}
